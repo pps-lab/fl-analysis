29d5a74badfe4dc5bc3c79b544b8e5651458c01f
Hidde Lycklama <mail.hidde@gmail.com>
2021-09-13 15:19:18 +0200

    Mobilenet

--- src/model/mobilenet.py
+++ src/model/mobilenet.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 def mobilenetv2_cifar10():
     return tf.keras.applications.mobilenet_v2.MobileNetV2(
-        input_shape=(32, 32, 3), alpha=0.5,
+        input_shape=(32, 32, 3), alpha=0.65,
         include_top=True, weights=None, input_tensor=None, pooling=None,
         classes=10
     )
